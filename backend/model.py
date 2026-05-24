1df24925f9b3c5aec762c19eda29ebd9efb6ef2a
KevinYuvi <kevinyuviuce@gmail.com>
2026-05-24 14:40:19 -0500

    Agregar función de preprocesamiento de imágenes y actualizar la lógica de predicción

--- backend/model.py
+++ backend/model.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 import torch
 import torch.nn as nn
-from PIL import Image
+from PIL import Image, ImageOps
+import torch.nn.functional as F
 from torchvision import transforms
 
 BASE_DIR = Path(__file__).resolve().parent
@@ -44,9 +45,35 @@ model = CNN().to(device)
 model.load_state_dict(torch.load(MODEL_PATH, map_location=device))
 model.eval()
 
+def preprocess_image(image: Image.Image):
+    image = image.convert("L")
+
+    # Asegurar fondo negro y trazo blanco
+    image = ImageOps.invert(image) if image.getpixel((0, 0)) > 128 else image
+
+    # Recortar bordes vacíos
+    bbox = image.getbbox()
+    if bbox:
+        image = image.crop(bbox)
+
+    # Mantener proporción y meter en 20x20
+    image.thumbnail((20, 20), Image.Resampling.LANCZOS)
+
+    # Crear canvas MNIST 28x28
+    new_image = Image.new("L", (28, 28), 0)
+
+    x = (28 - image.width) // 2
+    y = (28 - image.height) // 2
+
+    new_image.paste(image, (x, y))
+
+    tensor = transforms.ToTensor()(new_image)
+    tensor = transforms.Normalize((0.1307,), (0.3081,))(tensor)
+
+    return tensor.unsqueeze(0)
 
 def predict_python(image: Image.Image):
-    image_tensor = transform(image).unsqueeze(0)
+    image_tensor = preprocess_image(image)
 
     with torch.no_grad():
         output = model(image_tensor)
